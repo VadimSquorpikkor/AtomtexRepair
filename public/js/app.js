4ccd4b8018ea0442aaae4d78c796c2b58aa8eee2
VadimSquorpikkor <uniqueehub1024>
2021-05-15 15:20:29 +0300

    1.03 - 15/05/21 1. Добавлены словари на 4 языка для перевода текста на странице. Теперь слова, которые выводятся JS кодом, автоматом переводятся в нужный язык 2. Изменены кнопки с флагами: теперь отображаются ВСЕ, флаг выбранного языка отображается увеличенным по сравнению с остальными 3. Немного переделаны надписи в списках найденных устройств

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,8 @@
+const NOTHING_FOUND = "nothing_found"
+const NO_STATES_FOUND = "no_states_found"
+const SERIAL = "serial"
+const DAYS_UNDER_REPAIR = "days_under_repair"
+const ACCEPTED_TO_REPAIR = "accepted_for_repair"
 
 function valueOfElement(id) {
     return document.getElementById(id).value
@@ -11,6 +16,22 @@ function rightDayString(i) {
         case 3:
         case 4:return "дня";
         default:return "дней";
+    }
+}
+
+
+let idDictionary  = [NOTHING_FOUND,                 NO_STATES_FOUND,         SERIAL,            DAYS_UNDER_REPAIR,      ACCEPTED_TO_REPAIR];
+let rusDictionary = ['Ничего не найдено',           'Событий не найдено',    'Серийный номер',  'Дней в ремонте',       'Принят в ремонт'];
+let engDictionary = ['Nothing found',               'No events found',       'Serial number',   'Days under repair',    'Accepted for repair'];
+let zhnDictionary = ['没有发现',                     '找不到事件',              '序列号',          '维修天数',               '接受维修'];
+let itaDictionary = ['Non abbiamo trovato nulla',   'Nessun evento trovato', 'Numero di serie', 'Giorni in riparazione','Accettato per la riparazione'];
+
+function getRightLanguage(id) {
+    switch (language) {
+        case LANG_RUSSIAN: return getNameById(id, rusDictionary, idDictionary);
+        case LANG_ENGLISH: return getNameById(id, engDictionary, idDictionary);
+        case LANG_CHINESE: return getNameById(id, zhnDictionary, idDictionary);
+        case LANG_ITALIAN: return getNameById(id, itaDictionary, idDictionary);
     }
 }
 
@@ -47,16 +68,18 @@ function addSerialDataRowToPage(arr) {
             data +=
             '<div class="found_unit_item" onclick=getAllEventsByUnitIdSmall("'+unit.id+'")>'+
             '    <div class="item_info_div">'+
-            '        <span class="big_orange">'+ deviceName +'</span>'+
-            '        <span class="small_white">№ '+ serial +'</span>'+
-            '        <span class="small_white">(вн. '+ innerSerial +')</span><br>'+
+            '        <span class="big_orange">'+ deviceName +'</span><br>'+
+            '        <span class="small_white">'+ getRightLanguage(SERIAL) + ' — ' + serial +'</span><br>'+
+            // '        <span class="small_white">(вн. '+ innerSerial +')</span><br>'+
             '        <span class="big_orange">'+ location +'</span><br>'+
-            '        <span class="small_white">'+ date +'</span>'+
             '        <span class="small_white">'+ state +'</span>'+
             '    </div>'+
             '    <div class="day_count_div">'+
-            '        <span class="big_orange">'+ daysCount +'</span><br>'+
-            '        <span class="small_white">'+ dayString +'</span>'+
+            '        <span class="small_white">'+ getRightLanguage(ACCEPTED_TO_REPAIR) +':</span><br>'+
+            '        <span class="small_white">'+ date +'</span><br>'+
+            // '        <span class="small_white">'+ dayString +'</span>'+
+            '        <span class="small_white">'+ getRightLanguage(DAYS_UNDER_REPAIR) +':</span><br>'+
+            '        <span class="big_orange">'+ daysCount +'</span>'+
             '    </div>'+
             '    <div id="'+STATE_PREF+unit.id+'" class="state_host"></div>'+
             '</div>';
@@ -73,14 +96,14 @@ function addSerialDataRowToPage(arr) {
  * @param id - id элемента, в который будет вставлено "Не найдено"
  */
 function insertNothing(id) {
-    document.getElementById(id).innerHTML = '<span class="white_span">'+FOUND_NOTHING+'</span>'
+    document.getElementById(id).innerHTML = '<span class="white_span">'+getRightLanguage(NOTHING_FOUND)+'</span>'
 }
 
 function addCollectionOfDocumentToDiv_new(arr, host) {
     let data;
     if (arr.length === 0) {
         document.getElementById(host).innerHTML =
-            '<span class="white_span">Статусов не найдено</span>';
+            '<span class="white_span">'+getRightLanguage(NO_STATES_FOUND)+'</span>';
     } else {
         let event;
         data = '';
